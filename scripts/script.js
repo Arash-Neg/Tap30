ac89e9dee518bef0223810970bc78c3c89c4b5c2
Arash <arash.negahdari89@gmail.com>
2024-03-17 20:01:25 +0330

    Hero Header Component - (icon not complete)

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
 
+/* Nav Toggler */
 toggler.addEventListener("click", (e) => {
   navbar.classList.toggle("nav__expanded");
 });
